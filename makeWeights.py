b5a843dadd9913d148027ef05360d9299d305602
Rishikanth <rishikanthc@gmail.com>
2019-04-08 01:36:43 -0700

    first commit

---
+++ makeWeights.py
@@ -0,0 +1,83 @@
+import sys
+from scipy import sparse
+import numpy as np
+
+DATASTRING = "double Val"
+ARRSTRING = "[] = {"
+ROWSTRING = "int RowPtr"
+COLSTRING = "int Col"
+TERMSTRING = "};\n"
+
+def loadWeights(fname):
+    matx = []
+    with open(fname) as fp:
+        for lines in fp:
+            w = np.array(lines.strip().split(' '), dtype=float)
+            matx.append(w)
+    
+    spmatx = sparse.csr_matrix(matx)
+    #print(spmatx.todense())
+    return spmatx
+
+def writeWeights(fp, sp_matx, idx):
+    dat = np.array2string(sp_matx.data,separator=',').strip('[]')
+    indices = np.array2string(sp_matx.indices,separator=',').strip('[]')
+    indptr = np.array2string(sp_matx.indptr,separator=',').strip('[]')
+
+
+    fp.write(COLSTRING + idx + ARRSTRING + indices + TERMSTRING)
+    fp.write(ROWSTRING + idx + ARRSTRING + indptr + TERMSTRING)
+    fp.write(DATASTRING + idx + ARRSTRING + dat + TERMSTRING)
+
+    #np.savetxt(fname + '_indices', sp_matx.indices)
+    #np.savetxt(fname + '_indptr', sp_matx.indptr)
+    #np.savetxt(fname + '_data', sp_matx.data)
+
+def writeFullWeights(fp, fullmat, idx):
+    if idx == 0:
+        fp.write("double fweight" + str(idx) + "[][VECSIZE] = {")
+    else:
+        fp.write("double fweight" + str(idx) + "[][WSIZE] = {")
+
+    for arr in fullmat:
+        data = np.array2string(arr, separator=',').strip('][')
+        fp.write(data+",")
+    fp.write(TERMSTRING)
+
+def genSparse(m, n, nz):
+    matx = np.random.rand(m, n)
+    fullmat = matx
+
+    sp = int(m * n * nz / 100)
+    for i in range(sp):
+        ridx = np.random.randint(m)
+        cidx = np.random.randint(100) % n
+        matx[ridx][cidx] = 0.0
+
+    matx = sparse.csr_matrix(matx)
+    print("generated sparse matrix")
+    print(matx.todense())
+    return matx, fullmat
+    
+if __name__ == '__main__':
+    fp = open('sparseWeights.h', 'w')
+    fp.write("#define WSIZE " + str(sys.argv[1]) + '\n')
+    fp.write("#define VECSIZE " + str(4) + '\n')
+    w0, wf0 = genSparse(int(sys.argv[1]), 4, int(sys.argv[2]))
+    w1, wf1 = genSparse(4, int(sys.argv[1]), int(sys.argv[2]))
+    writeWeights(fp, w0, '0')
+    writeWeights(fp, w1, '1')
+    arr = np.random.rand(4)
+    vector = np.array2string(arr, separator=',').strip('[]')
+    fp.write("double vector[VECSIZE] = {" + vector + "};\n")
+    writeFullWeights(fp, wf0, 0)
+    writeFullWeights(fp, wf1, 1)
+    fp.close()
+    t = wf0.dot(arr)
+    print(wf1.dot(t))
+    #w0 = loadWeights(sys.argv[1])
+    #writeWeights('sp_weights0', w0)
+    #w1 = loadWeights(sys.argv[2])
+    #writeWeights('sp_weights1', w1)
+    #w1 = loadWeights(sys.argv[2])
+
---
+++ mul.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "sparseWeights.h"
+#include <string.h>
+
+void MatrixVectorMult(double out[], double Val[], int RowPtr[], int Col[],
+		      double in[], int m)
+{
+   int i, k;
+
+   for (k = 0; k < m; k = k + 1) {
+      out[k] = 0.0;
+   }
+
+   for (i = 0; i < m; i = i + 1) {
+      for (k = RowPtr[i]; k < RowPtr[i+1]; k = k + 1) {
+         // cout << "k = " << k << ", Col[k] = " << Col[k] 
+         //		<< ", in[Col[k]] = " << in[Col[k]] << endl;
+         // cout << Val[k] << " * " << in[Col[k]] << endl;
+
+         out[i] = out[i] + Val[k]*in[Col[k]];
+      }
+   }
+}
+
+void fullMatrixMult(int layer, double vec[], double out[], int n, int m){
+	int i,j;
+
+	if (layer == 0)
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				out[i] = out[i] + fweight0[i][j]*vec[j];
+	else
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				out[i] = out[i] + fweight1[i][j]*vec[j];
+
+
+
+ }
+
+void opMatrixMult(int layer, double vec[], double out[], int n) {
+	if (layer == 0)
+		for (int i = 0; i < n; i++) {
+			out[i] = fweight0[i][0] * vec[0] + fweight0[i][1] * vec[1] + fweight0[i][2] * vec[2] + fweight0[i][3] * vec[3];
+		}
+	else
+		for (int i = 0; i < n; i++) {
+			out[i] = fweight1[i][0] * vec[0] + fweight1[i][1] * vec[1] + fweight1[i][2] * vec[2] + fweight1[i][3] * vec[3];
+		}
+
+
+}
+
+int main(int argc, char ** argv)
+{
+   /*double Val[] = {11, 12, 14, 22, 23, 25, 31, 33, 34, 42, 45,
+		46, 55, 65, 66, 67, 75, 77, 78, 87, 88};
+   int RowPtr[] = {0, 3, 6, 9, 12, 13, 16, 19, 21};
+   int Col[] = {0, 1, 3, 1, 2, 4, 0, 2, 3, 1, 4,
+		5, 4, 4, 5, 6, 4, 6, 7, 6, 7};*/
+   double output[VECSIZE], temp[WSIZE], fullout[VECSIZE];
+   int i, j;
+
+   //u[0] = 3; u[1] = 5; u[2] = 2; u[3] = 1;
+   //u[4] = 0; u[5] = 1; u[6] = 2; u[7] = 4;
+
+
+   MatrixVectorMult(temp, Val0, RowPtr0, Col0, vector, WSIZE);
+   MatrixVectorMult(output, Val1, RowPtr1, Col1, temp, VECSIZE);
+
+   for (i = 0; i < VECSIZE; i = i + 1) {
+       printf("      %4.1lf\n", output[i]);
+   }
+
+   memset(output, 0.0, VECSIZE*sizeof(double));
+   memset(temp, 0.0, WSIZE*sizeof(double));
+   for (i = 0; i < VECSIZE; i = i + 1) {
+       printf("      %4.1lf\n", output[i]);
+   }
+
+   fullMatrixMult(0, vector, temp, VECSIZE, WSIZE);
+   fullMatrixMult(1, temp, output, WSIZE, VECSIZE);
+
+   printf("\n full mult\n");
+   for (i = 0; i < VECSIZE; i = i + 1) {
+       printf("      %4.1lf\n", output[i]);
+   }
+}
+
